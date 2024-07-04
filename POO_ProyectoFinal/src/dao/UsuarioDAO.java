02f9d44c7b00880953c91c833024e252867a59fc
roman <roman@HP>
2024-07-04 12:03:22 -0500

    documentación de la clase UsuarioDAO

--- POO_ProyectoFinal/src/dao/UsuarioDAO.java
+++ POO_ProyectoFinal/src/dao/UsuarioDAO.java
@@ -1,21 +1,21 @@
 package dao;
 
-import model.Usuario;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import model.Conexion;
+import model.Usuario; //importamos esta clase para almacenar el valor de retorno del método obtenerUsuarioPorCredenciales
+import java.sql.Connection; //clase usada para almacenar el resultado de la conexión a la BD (el método establecido en la clase Conexion)
+import model.Conexion;//clase creada para colocar todo lo referente a la configuración de la conexión y establecer el método que genere la conexión a la BD
 
+import java.sql.PreparedStatement; //clase para preparar una consulta en la base de datos (BD)
+import java.sql.ResultSet; //clase para obtener el resultado de la consulta
 
 public class UsuarioDAO {
     public Usuario obtenerUsuarioPorCredenciales(String usuario, String contraseña) {
     Usuario user = null; // Variable para almacenar el usuario encontrado (si existe).
-    Conexion conexion = new Conexion(); // Crear una instancia de la clase Conexion.
+    Conexion conexion = new Conexion(); // Crear una instancia de la clase Conexio para hacer uso de su método y establecer la conexión
     Connection con = conexion.obtenerConexion(); // Obtener una conexión a la base de datos.
 
     if (con != null) { // Verificar si la conexión se estableció correctamente.
         try {
-            // Query SQL para buscar el usuario en la base de datos.
+            // Query SQL para buscar el usuario en la BD ('?' simboliza los valores a reemplazar: el usuario y contraseña ingresados)
             String query = "SELECT * FROM Usuarios WHERE usuario = ? AND contraseña = ?";
             
             // Preparar el statement para ejecutar la consulta SQL.
